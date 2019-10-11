580a07bec74b056acb1d160192be58c101e56ed5
ralphu <ralphutz@gmail.com>
2019-10-11 14:23:45 -0500

    rectangles_method_refactor

--- rectangles/src/main.rs
+++ rectangles/src/main.rs
@@ -5,6 +5,12 @@ struct Rectangle {
     heigth: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.heigth
+    }
+}
+
 fn main() {
     let rect1 = Rectangle {
         width: 30,
@@ -14,10 +20,6 @@ fn main() {
     println!("rect1 is {:?}", rect1);
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rect1)
+        rect1.area()
     );
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.heigth
 }
